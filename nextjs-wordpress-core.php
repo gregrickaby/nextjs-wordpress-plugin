ca424753788e03fb6e6f8301aa07bb8e97e8880c
Greg Rickaby <gregrickaby@gmail.com>
2022-09-18 14:57:48 -0500

    initial commit

---
+++ nextjs-wordpress-core.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: Next.js WordPress Core Plugin
+ * Plugin URI:  https://github.com/gregrickaby/nextjs-wordpress-core-plugin
+ * Description: A plugin to help turn WordPress into a headless CMS.
+ * Version:     1.0.0
+ * Author:      Greg Rickaby <greg@gregrickaby.com>
+ * Author URI:  https://gregrickaby.com
+ * License:     MIT
+ *
+ * @package NEXTJS_HEADLESS_CORE
+ */
+
+namespace NEXTJS_HEADLESS_CORE;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+// Define constants.
+define( 'NEXTJS_HEADLESS_CORE_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'NEXTJS_HEADLESS_CORE_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'NEXTJS_HEADLESS_CORE_VERSION', '1.0.0' );
+define( 'NEXTJS_HEADLESS_CORE_OPTION_NAME', 'headless_config' );
+
+// Require files.
+require_once 'inc/blocks.php';
+require_once 'inc/editor.php';
+require_once 'inc/links.php';
+require_once 'inc/media.php';
+require_once 'inc/menus.php';
+require_once 'inc/revalidation.php';
+require_once 'inc/wp-graphql.php';
+require_once 'inc/yoast.php';
