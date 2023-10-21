b30eb14224cd93b88f0d3d470daa419d7bc1ea80
Greg Rickaby <gregrickaby@gmail.com>
2023-10-21 15:47:29 -0500

    update constants. remove required files. bump version

--- nextjs-wordpress-plugin.php
+++ nextjs-wordpress-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Next.js WordPress Plugin
  * Plugin URI:  https://github.com/gregrickaby/nextjs-wordpress-plugin
  * Description: A plugin to help turn WordPress into a headless CMS.
- * Version:     1.0.3
+ * Version:     1.0.4
  * Author:      Greg Rickaby <greg@gregrickaby.com>
  * Author URI:  https://gregrickaby.com
  * License:     MIT
@@ -21,14 +21,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Define constants.
 define( 'NEXTJS_WORDPRESS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'NEXTJS_WORDPRESS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'NEXTJS_WORDPRESS_VERSION', '1.0.3' );
+define( 'NEXTJS_WORDPRESS_PLUGIN_VERSION', '1.0.4' );
 
 // Require files.
 require_once 'inc/blocks.php';
-require_once 'inc/editor.php';
 require_once 'inc/links.php';
-require_once 'inc/media.php';
-require_once 'inc/menus.php';
 require_once 'inc/revalidation.php';
 require_once 'inc/wp-graphql.php';
 require_once 'inc/yoast.php';
