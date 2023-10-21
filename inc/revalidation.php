d0ec54839ff9d042b2351c30ccce76c5b02484a3
Greg Rickaby <gregrickaby@gmail.com>
2023-10-21 15:47:43 -0500

    refactor on demand revalidation for next.js 13

--- inc/revalidation.php
+++ inc/revalidation.php
@@ -9,25 +9,34 @@
 
 namespace NEXTJS_WORDPRESS_PLUGIN;
 
-use \WP_Post;
+/**
+ * Handle post status transition.
+ *
+ * @param string $new_status New status.
+ * @param string $old_status Old status.
+ */
+function transition_handler( $new_status, $old_status ): void {
+	// If the post is a draft, bail.
+	if ( 'draft' === $new_status && 'draft' === $old_status ) {
+		return;
+	}
+
+	// Otherwise, revalidate.
+	on_demand_revalidation();
+}
+add_action( 'transition_post_status', __NAMESPACE__ . '\transition_handler', 10, 3 );
 
 /**
  * Flush the frontend cache when a post is updated.
  *
  * This function will fire anytime a post is updated.
  * Including: the post status, comments, meta, terms, etc.
- * The slug MUST have a leading slash and cannot have a railing slash!
  *
- * @usage https://gregrickaby.com/api/wordpress/revalidate?token=123&slug=/the-slug
+ * @usage https://nextjswp.com/api/revalidate
  *
- * @see https://developer.wordpress.org/reference/hooks/edit_post/
- * @see https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration#using-on-demand-revalidation
- * @since 1.0.0
- * @author Greg Rickaby
- * @param int     $post_ID  The post ID.
- * @param WP_Post $post The post object.
+ * @see https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#on-demand-revalidation
  */
-function on_demand_revalidation( $post_ID, WP_Post $post ) {
+function on_demand_revalidation(): void {
 
 	// Do not run on autosave.
 	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
@@ -35,36 +44,26 @@ function on_demand_revalidation( $post_ID, WP_Post $post ) {
 	}
 
 	// No constants or Post ID? Bail...
-	if ( ! defined( 'HEADLESS_FRONTEND_URL' ) || ! defined( 'PREVIEW_SECRET_TOKEN' ) || ! $post_ID ) {
+	if ( ! defined( 'NEXTJS_FRONTEND_URL' ) || ! defined( 'NEXTJS_REVALIDATION_SECRET' ) ) {
 		return;
 	}
-
-	// Build and format the slug.
-	$slug = \get_the_permalink( $post_ID );
-	$slug = str_replace( home_url(), '', $slug );
-	$slug = ltrim( $slug, '/' );
-	$slug = rtrim( $slug, '/' );
-	$slug = '/' . $slug;
 
 	// Build the API URL.
-	$api_url = HEADLESS_FRONTEND_URL . '/api/wordpress/revalidate';
-	$api_url = \add_query_arg( 'token', PREVIEW_SECRET_TOKEN, $api_url );
-	$api_url = \add_query_arg( 'slug', $slug, $api_url );
+	$revalidation_url = esc_url_raw( NEXTJS_FRONTEND_URL . 'api/revalidate' );
 
 	// Make API request.
-	$response = \wp_remote_post( esc_url_raw( $api_url ), [ 'blocking' => true ] );
+	$response = wp_remote_post(
+		$revalidation_url,
+		[
+			'headers' => [
+				'x-vercel-revalidation-secret' => NEXTJS_REVALIDATION_SECRET,
+			],
+		]
+	);
 
 	// Check the response.
-	$response_code    = \wp_remote_retrieve_response_code( $response );
-	$response_message = \wp_remote_retrieve_response_message( $response );
-
-	// Error handle the response.
-	if ( 200 !== $response_code && ! empty( $response_message ) ) {
-		return new \WP_Error( 'grd-headless-core-revalidation-error', $response_message );
-	} elseif ( 200 !== $response_code ) {
-		return new \WP_Error( 'grd-headless-core-revalidation-error', sprintf( 'GRD Headless Core: Error %d: %s', $response_code, $response_message ) );
-	} else {
-		return;
+	if ( is_wp_error( $response ) ) {
+		$error_message = $response->get_error_message();
+		error_log( 'Revalidation error: ' . $error_message );
 	}
 }
-add_action( 'edit_post', __NAMESPACE__ . '\on_demand_revalidation', 10, 3 );
