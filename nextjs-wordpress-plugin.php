77ce88b5c32cbb4632003a15c760395e0a4ac376
Greg Rickaby <gregrickaby@gmail.com>
2024-01-30 14:18:40 -0600

    maybe autoload classes. version bump.

--- nextjs-wordpress-plugin.php
+++ nextjs-wordpress-plugin.php
@@ -3,15 +3,15 @@
  * Plugin Name: Next.js WordPress Plugin
  * Plugin URI:  https://github.com/gregrickaby/nextjs-wordpress-plugin
  * Description: A plugin to help turn WordPress into a headless CMS.
- * Version:     1.0.4
+ * Version:     1.0.6
  * Author:      Greg Rickaby <greg@gregrickaby.com>
  * Author URI:  https://gregrickaby.com
  * License:     MIT
  *
- * @package NEXTJS_WORDPRESS_PLUGIN
+ * @package NextJS_WordPress_Plugin
  */
 
-namespace NEXTJS_WORDPRESS_PLUGIN;
+namespace NextJS_WordPress_Plugin;
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -21,11 +21,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Define constants.
 define( 'NEXTJS_WORDPRESS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'NEXTJS_WORDPRESS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'NEXTJS_WORDPRESS_PLUGIN_VERSION', '1.0.4' );
+define( 'NEXTJS_WORDPRESS_PLUGIN_VERSION', '1.0.6' );
 
 // Require files.
-require_once 'inc/blocks.php';
-require_once 'inc/links.php';
-require_once 'inc/revalidation.php';
-require_once 'inc/wp-graphql.php';
-require_once 'inc/yoast.php';
+$autoload_file = __DIR__ . '/vendor/autoload.php';
+if ( file_exists( $autoload_file ) ) {
+	require $autoload_file;
+} else {
+	require_once 'src/classes/Blocks.php';
+	require_once 'src/classes/Links.php';
+	require_once 'src/classes/Plugin.php';
+	require_once 'src/classes/Revalidation.php';
+	require_once 'src/classes/YoastSEO.php';
+}
+
+// Initialize the plugin.
+$nextjs_wordpress_plugin = new Plugin();
